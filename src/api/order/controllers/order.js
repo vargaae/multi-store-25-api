eeee3315b830ea4f87562a72c7fae1b81bde5a45
vargaae <vargaae@hotmail.com>
2024-07-30 23:40:23 +0200

    303-05-Set Strapi-STRIPE success url

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -34,8 +34,8 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
         shipping_address_collection: { allowed_countries: ["HU", "US", "CA"] },
         payment_method_types: ["card"],
         mode: "payment",
-        success_url: process.env.CLIENT_URL + "success=true",
-        cancel_url: process.env.CLIENT_URL + "success=false",
+        success_url: process.env.CLIENT_URL + "/success=true",
+        cancel_url: process.env.CLIENT_URL + "",
         line_items: lineItems,
       });
 
